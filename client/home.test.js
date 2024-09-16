38b3afb4f508bb694890f1d2ca27c620b0a1cb63
Chad <107705900+carrchadd@users.noreply.github.com>
2024-09-15 21:35:24 -0400

    change v.3

---
+++ client/home.test.js
@@ -0,0 +1,11 @@
+const { math_operations } = require("./home");
+
+describe("home js test", () => {
+  // it("should test json  ", async () => {
+  //   expect(await print_json()).toBe(true);
+  // });
+
+  it("should test arithmetic", () => {
+    expect(math_operations(1, 4, 6, 3)).toBe(73)
+  });
+});
