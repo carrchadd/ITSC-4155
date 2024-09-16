64c4e1a66ed6f8084613ccbcd28d3f3a2a4c3365
Chad <107705900+carrchadd@users.noreply.github.com>
2024-09-15 21:46:30 -0400

    change v.5

--- home.test.js
+++ home.test.js
@@ -6,6 +6,6 @@ describe("home js test", () => {
   // });
 
   it("should test arithmetic", () => {
-    expect(math_operations(1, 4, 6, 3)).toBe(74)
+    expect(math_operations(1, 4, 6, 3)).toBe(73)
   });
 });
