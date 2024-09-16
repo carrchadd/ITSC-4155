9c8293e65d4ae12ab95ed03fffafa3e184620615
Chad <107705900+carrchadd@users.noreply.github.com>
2024-09-15 23:22:02 -0400

    testing pipeline

--- client/home.test.js
+++ client/home.test.js
@@ -3,6 +3,6 @@ const { math_operations } = require("./home");
 describe("client math js test", () => {
 
   it("should test arithmetic", () => {
-    expect(math_operations(1, 1, 1, 1)).toBe(3)
+    expect(math_operations(1, 1, 1, 1)).toBe(2)
   });
 });
