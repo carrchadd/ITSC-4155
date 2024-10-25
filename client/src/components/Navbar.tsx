526cea42c0c09ec74dde100350da926944e165c6
Chad <107705900+carrchadd@users.noreply.github.com>
2024-10-24 22:42:10 -0400

    Test

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 const Navbar = () => {
   return (
     <div className='flex items-center h-24 max-w-[1240px] mx-auto'>
-      <p className='text-3xl text-white mx-auto font-serif italic'>Taking Input Demo</p>
+      <p className='text-3xl text-blue-600 mx-auto font-serif italic'>Taking Input Demo</p>
     </div>
   )
 }
