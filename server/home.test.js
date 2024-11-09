9ca5c5aab83996f45c7666d4a8de38accfc2c631
Chad <107705900+carrchadd@users.noreply.github.com>
2024-11-08 21:39:57 -0500

    Checking test

--- server/home.test.js
+++ server/home.test.js
@@ -2,12 +2,18 @@ const { print_json, math_operations } = require("./home");
 
 describe("server json js test", () => {
   it("should test json  ",  () => {
-    expect( print_json()).toBe(true);
+    expect(print_json()).toBe(true);
   });
 });
 
 describe("server math js test", () => {
   it("should test arithmetic", () => {
     expect(math_operations(2, 2, 2, 2)).toBe(10)
+  });
+});
+
+describe("new test", () => {
+  it("should test arithmetic", () => {
+    expect(math_operations(1, 1, 1, 1)).toBe(2)
   });
 });
